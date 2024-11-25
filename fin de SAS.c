ac01768e860c810ea13ecb6530e18ebcce311397
Bassma <elabassibasma31@gmail.com>
2024-11-26 00:32:49 +0100

    Création de la fonction ajouter une tâche

--- fin de SAS.c
+++ fin de SAS.c
@@ -15,3 +15,31 @@ struct tache
 	struct Date date_sep;
 	int prioritaire;
 };
+
+void ajoutez_tache(struct tache *tache)
+{
+	printf("Entrer titre : ");
+	scanf(" %[^\n]%*c", tache->titre);
+
+	printf("Entrer description : ");
+	scanf(" %[^\n]%*c", tache->description);
+
+	printf("Entrer jour : ");
+	scanf("%d", &tache->date_sep.jour);
+
+	printf("Entrer mois : ");
+	scanf("%d", &tache->date_sep.mois);
+
+	do
+	{
+		printf("Entrer annee (4 chiffres) : ");
+		scanf("%d", &tache->date_sep.annee);
+		if (tache->date_sep.annee < 1000 || tache->date_sep.annee > 9999)
+		{
+			printf("L'annee doit contenir exactement 4 chiffres.\n");
+		}
+	} while (tache->date_sep.annee < 1000 || tache->date_sep.annee > 9999);
+
+	printf("Entrer le niveau de priorite (1 = haut, 0 = bas) : ");
+	scanf("%d", &tache->prioritaire);
+}
